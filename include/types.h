0b185f41c22d71cef942f0f243f9a269fef1788d
Justin Schwartz <zero0@users.noreply.github.com>
2024-01-25 23:55:33 -0800

    Added 32bit types and made sure ptr_t matches void* size

--- include/types.h
+++ include/types.h
@@ -1,16 +1,21 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned int ptr_t;
-typedef signed int ptrdiff_t;
+#include "macros.h"
 
-typedef unsigned char uint8_t;
-typedef unsigned int uint16_t;
+typedef unsigned int    ptr_t;
+typedef signed int      ptrdiff_t;
+STATIC_ASSERT(sizeof(ptr_t) == sizeof(void*));
 
-typedef signed char int8_t;
-typedef signed int int16_t;
+typedef unsigned char   uint8_t;
+typedef unsigned int    uint16_t;
+typedef unsigned long   uint32_t;
 
-typedef unsigned char pixel_t;
+typedef signed char     int8_t;
+typedef signed int      int16_t;
+typedef signed long     int32_t;
+
+typedef unsigned char   pixel_t;
 
 typedef struct
 {
